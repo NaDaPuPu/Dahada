5bdae718a88fcbdf94a534f802694d81b91fa069
NaDaPuPu <jun19998@gmail.com>
2020-04-04 20:18:32 +0900

    Add register branch

--- dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
@@ -2,7 +2,12 @@ package com.d2w.dahada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.d2w.dahada.data.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.toLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
