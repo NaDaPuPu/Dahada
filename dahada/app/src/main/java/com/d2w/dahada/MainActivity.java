53373eb07e637fb5c1b38e2fa4ce461639d60c95
NaDaPuPu <jun19998@gmail.com>
2020-04-20 00:21:11 +0900

    Complete Bottom Bar, Delete Action Bar

--- dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.view.MenuItem;
@@ -12,7 +13,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.d2w.dahada.data.bottombar.Calender;
+import com.d2w.dahada.data.bottombar.Community;
 import com.d2w.dahada.data.bottombar.Mypage;
+import com.d2w.dahada.data.bottombar.Notice;
 import com.d2w.dahada.data.login.LoginActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,6 +23,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     Calender calender;
+    Community community;
+    Notice notice;
     Mypage mypage;
 
     @Override
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calender = new Calender();
+        community = new Community();
+        notice = new Notice();
         mypage = new Mypage();
 
         /*getSupportFragmentManager().beginTransaction().replace(R.id.container, calender).commit();*/
@@ -37,16 +44,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
+                    case R.id.tab1:
+                        Toast.makeText(getApplicationContext(), "홈", Toast.LENGTH_LONG).show();
+                        findViewById(R.id.container).setVisibility(View.INVISIBLE);
+                        return true;
+
                     case R.id.tab2:
                         Toast.makeText(getApplicationContext(), "캘린더 화면", Toast.LENGTH_LONG).show();
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, calender).commit();
+                        getSupportFragmentManager().beginTransaction().commit();
+                        findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        return true;
 
+                    case R.id.tab3:
+                        Toast.makeText(getApplicationContext(), "커뮤니티", Toast.LENGTH_LONG).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, community).commit();
+                        getSupportFragmentManager().beginTransaction().commit();
+                        findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        return true;
+
+                    case R.id.tab4:
+                        Toast.makeText(getApplicationContext(), "알림", Toast.LENGTH_LONG).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notice).commit();
+                        getSupportFragmentManager().beginTransaction().commit();
+                        findViewById(R.id.container).setVisibility(View.VISIBLE);
                         return true;
 
                     case R.id.tab5:
                         Toast.makeText(getApplicationContext(), "마이페이지", Toast.LENGTH_LONG).show();
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, mypage).commit();
-
+                        getSupportFragmentManager().beginTransaction().commit();
+                        findViewById(R.id.container).setVisibility(View.VISIBLE);
                         return true;
                 }
                 return false;
