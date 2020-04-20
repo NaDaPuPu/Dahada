be53920f5834bfd5804a2c23bc7e0f952513d7a4
NaDaPuPu <jun19998@gmail.com>
2020-04-20 14:10:25 +0900

    Edit Bottom Bar, Change Main Activity, Delete Home_DaeGeun

--- dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
@@ -2,11 +2,8 @@ package com.d2w.dahada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
-import android.widget.Button;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -16,7 +13,6 @@ import com.d2w.dahada.data.bottombar.Calender;
 import com.d2w.dahada.data.bottombar.Community;
 import com.d2w.dahada.data.bottombar.Mypage;
 import com.d2w.dahada.data.bottombar.Notice;
-import com.d2w.dahada.data.login.LoginActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -47,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.tab1:
                         Toast.makeText(getApplicationContext(), "홈", Toast.LENGTH_LONG).show();
                         findViewById(R.id.container).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.main_Layout).setVisibility(View.VISIBLE);
                         return true;
 
                     case R.id.tab2:
@@ -54,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, calender).commit();
                         getSupportFragmentManager().beginTransaction().commit();
                         findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        findViewById(R.id.main_Layout).setVisibility(View.INVISIBLE);
                         return true;
 
                     case R.id.tab3:
@@ -61,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, community).commit();
                         getSupportFragmentManager().beginTransaction().commit();
                         findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        findViewById(R.id.main_Layout).setVisibility(View.INVISIBLE);
                         return true;
 
                     case R.id.tab4:
@@ -68,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, notice).commit();
                         getSupportFragmentManager().beginTransaction().commit();
                         findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        findViewById(R.id.main_Layout).setVisibility(View.INVISIBLE);
                         return true;
 
                     case R.id.tab5:
@@ -75,29 +75,10 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, mypage).commit();
                         getSupportFragmentManager().beginTransaction().commit();
                         findViewById(R.id.container).setVisibility(View.VISIBLE);
+                        findViewById(R.id.main_Layout).setVisibility(View.INVISIBLE);
                         return true;
                 }
                 return false;
-            }
-        });
-//캘린더부분
-        Button button = (Button) findViewById(R.id.toLogin);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button button1 = findViewById(R.id.toHome);
-        button1.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getApplicationContext(), Home_DaeGeun.class);
-                startActivity(intent);
             }
         });
     }
