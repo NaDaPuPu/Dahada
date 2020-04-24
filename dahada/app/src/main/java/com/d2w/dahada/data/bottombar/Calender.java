0dd0b46e449b3f8e7b7377f49a6a316e0ce6bdae
이채린 <201821074@sangmyung.kr>
2020-04-24 21:59:02 +0900

    add calendarview

--- dahada/app/src/main/java/com/d2w/dahada/data/bottombar/Calender.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/bottombar/Calender.java
@@ -1,19 +1,47 @@
 package com.d2w.dahada.data.bottombar;
 
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
+
 import android.view.ViewGroup;
 
+import android.widget.CalendarView;
+
+import android.widget.Toast;
+
+
 import androidx.fragment.app.Fragment;
+import com.d2w.dahada.R;
 
 import com.d2w.dahada.R;
+
+import java.util.Calendar;
 
 public class Calender extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.calender, container, false);
+    }
+//프래그먼트 이동코드
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+
+
+        //CalendarView 인스턴스 만들기
+
+        CalendarView calendar = (CalendarView)findViewById(R.id.calendar);
+
+
     }
 
 }
