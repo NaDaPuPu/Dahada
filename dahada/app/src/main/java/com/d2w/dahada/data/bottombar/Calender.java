afc1b85d962437780560d8f30e2ce16a6ebb358e
이채린 <201821074@sangmyung.kr>
2020-04-28 01:12:39 +0900

    캘린더뷰

--- dahada/app/src/main/java/com/d2w/dahada/data/bottombar/Calender.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/bottombar/Calender.java
@@ -9,9 +9,12 @@ import android.view.ViewGroup;
 
 import android.widget.CalendarView;
 
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.d2w.dahada.R;
 
@@ -21,27 +24,32 @@ import java.util.Calendar;
 
 public class Calender extends Fragment {
 
+
+//프래그먼트 이동코드
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        calendarView = (CalendarView) getView().findViewById(R.id.CalendarView);
+        myDate = (TextView) getView().findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int i, int i1, int i2) {
+                String data = (i1 + 1) + "/" + i2 + "/" + i;
+                myDate.setText(data);
+            }
+        });
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.calender, container, false);
     }
-//프래그먼트 이동코드
-
-    @Override
-
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.main);
-
-
-
-        //CalendarView 인스턴스 만들기
-
-        CalendarView calendar = (CalendarView)findViewById(R.id.calendar);
-
-
-    }
-
 }
