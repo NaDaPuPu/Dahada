f48b5572cb67ddd851c078715c076115aee2709b
NaDaPuPu <jun19998@gmail.com>
2020-04-29 12:40:53 +0900

    merge and debugging

--- dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
