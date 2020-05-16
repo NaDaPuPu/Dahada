925c18514deefd8076c3e2c995605dda4e3a934b
NaDaPuPu <jun19998@gmail.com>
2020-05-16 19:35:08 +0900

    공지사항 리사이클러뷰 추가

---
+++ dahada/app/src/main/java/com/d2w/dahada/NoticePost.java
@@ -0,0 +1,14 @@
+package com.d2w.dahada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticePost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_post);
+    }
+}
