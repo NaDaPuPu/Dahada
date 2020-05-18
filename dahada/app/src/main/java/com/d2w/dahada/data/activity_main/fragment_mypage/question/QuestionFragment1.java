2938564ab0294db10ab4816d5a07ddd4e9dbb648
NaDaPuPu <jun19998@gmail.com>
2020-05-19 01:36:12 +0900

    문의하기 레이아웃 완성(텍스트 추가)

--- dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_mypage/question/QuestionFragment1.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_mypage/question/QuestionFragment1.java
@@ -7,18 +7,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.d2w.dahada.R;
 
 public class QuestionFragment1 extends Fragment {
+    EditText content, emailText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_question_1, container, false);
 
         Spinner spinner = v.findViewById(R.id.spinner);
+
+        content = v.findViewById(R.id.content);
+        emailText = v.findViewById(R.id.emailText);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 문제발생일시 : \n" +
+                                "- 문의내용 : ");
+                        break;
+                    case 1:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 문제발생일시 : \n" +
+                                "- 문의내용 : ");
+                        break;
+                    case 2:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 오류 문구 창 내 메시지 확인 : \n" +
+                                "- 문제발생일시 : \n" +
+                                "- 문의내용 : ");
+                        break;
+                    case 3:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 오류 문구 창 내 메시지 확인 : \n" +
+                                "- 문제발생상품 :\n" +
+                                "- 문제발생일시 :\n" +
+                                "- 문의내용 : ");
+                        break;
+                    case 4:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 문제발생게시판 : \n" +
+                                "- 문제발생일시 : \n" +
+                                "- 문의내용 :");
+                        break;
+                    case 5:
+                        content.setText("아래 내용을 함께 보내주시면 더욱 빨리 처리가 가능합니다.\n" +
+                                "- 오류 문구 창 내 메시지 확인 : \n" +
+                                "- 문제발생일시 : \n" +
+                                "- 문의내용 : ");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         return v;
     }
 }
