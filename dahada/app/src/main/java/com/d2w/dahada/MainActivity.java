775f10fae5ba8151eb5df739587fd0a21b6ceeb6
NaDaPuPu <jun19998@gmail.com>
2020-05-21 02:02:01 +0900

    앱 시작 시 배너 화면 추가

--- dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -50,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
 
         main = new Main();
         calender = new Calender();
