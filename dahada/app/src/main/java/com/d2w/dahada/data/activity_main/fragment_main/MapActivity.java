f0fea67943ea3ddd87b0617be75b65a72d598d62
NaDaPuPu <jun19998@gmail.com>
2020-05-22 23:58:29 +0900

    Kakao Map API 추가

--- dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/MapActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/MapActivity.java
@@ -1,15 +1,23 @@
 package com.d2w.dahada.data.activity_main.fragment_main;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.d2w.dahada.R;
+
+import net.daum.mf.map.api.MapView;
 
 public class MapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_exercise_map);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
