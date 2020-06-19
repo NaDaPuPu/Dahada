9fc417732f4b185c65717b48c177d277877b2a84
Jangdae <201421320@sangmyung.kr>
2020-06-19 14:36:27 +0900

    이미지 업로드 메시지까지 완성

--- dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/community/PhotoPost_Activity.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/community/PhotoPost_Activity.java
@@ -1,16 +1,139 @@
 package com.d2w.dahada.data.activity_main.fragment_main.community;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.d2w.dahada.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class PhotoPost_Activity extends AppCompatActivity {
+
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+
+    private static final int Galley_Pick = 1;
+    private Uri ImageUri;
+    private String Description;
+    private StorageReference PostsImageRefrence;
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_post_);
+
+        PostsImageRefrence = FirebaseStorage.getInstance().getReference();
+
+        SelectPostImage = (ImageButton)findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button)findViewById(R.id.update_post_button);
+        PostDescription = (EditText)findViewById(R.id.post_description);
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Opengalley();
+            }
+        });
+
+        UpdatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidatePostInfo();
+            }
+        });
+
     }
+
+    private void ValidatePostInfo() {
+        Description = PostDescription.getText().toString();
+        if(ImageUri == null)
+        {
+            Toast.makeText(this,"이미지를 선택해 주세요.", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Description))
+        {
+            Toast.makeText(this,"코멘트를 달아주세요.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            StoringImageToStorage();
+        }
+    }
+
+    private void StoringImageToStorage() {
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        Calendar calForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+
+        StorageReference filePath = PostsImageRefrence.child("Post Images").child(ImageUri.getLastPathSegment() + postRandomName + ".jpg");
+
+        filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(PhotoPost_Activity.this, "이미지가 업로드 되었습니다!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(PhotoPost_Activity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void Opengalley() {
+        Intent galleyIntent = new Intent();
+        galleyIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleyIntent.setType("image/*");
+        startActivityForResult(galleyIntent, Galley_Pick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==Galley_Pick && resultCode==RESULT_OK && data!=null)
+        {
+            ImageUri = data.getData();
+            SelectPostImage.setImageURI(ImageUri);
+
+        }
+    }
+
+
 }
