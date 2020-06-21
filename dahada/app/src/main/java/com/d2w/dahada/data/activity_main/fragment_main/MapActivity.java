16ee4329710f8b033195757270761e0ee4f11ff5
NaDaPuPu <jun19998@gmail.com>
2020-06-22 03:40:03 +0900

    Edit 모드로 들어갔을 때만 Marker가 표시되도록 변경 저장, 취소 버튼 눌렀을 시 맵, ArrayList 클리어

--- dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/MapActivity.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/MapActivity.java
@@ -9,10 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -39,6 +41,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String TAG = "MapActivity";
     private GoogleMap map;
     private CameraPosition cameraPosition;
+
+    private ConstraintLayout courseEdit;
+    private Button courseButton, saveButton, cancelButton;
 
     private FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -76,8 +81,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
 
-        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
 
         fab1.setOnClickListener(this);
         fab2.setOnClickListener(this);
@@ -86,6 +91,48 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        courseEdit = findViewById(R.id.courseEdit);
+        courseButton = findViewById(R.id.courseButton);
+        saveButton = findViewById(R.id.saveButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        courseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab1.setVisibility(View.VISIBLE);
+                courseEdit.setVisibility(View.VISIBLE);
+                courseButton.setVisibility(View.GONE);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab1.setVisibility(View.INVISIBLE);
+                fab2.startAnimation(fab_close);
+                fab2.setClickable(false);
+                isFabOpen = false;
+                courseEdit.setVisibility(View.GONE);
+                courseButton.setVisibility(View.VISIBLE);
+                map.clear();
+                arrayPoints.clear();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab1.setVisibility(View.INVISIBLE);
+                fab2.startAnimation(fab_close);
+                fab2.setClickable(false);
+                isFabOpen = false;
+                courseEdit.setVisibility(View.GONE);
+                courseButton.setVisibility(View.VISIBLE);
+                map.clear();
+                arrayPoints.clear();
+            }
+        });
     }
 
     @Override
@@ -237,27 +284,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapClick(LatLng latLng) {
-        beforeLocation = latLng;
-        Log.d("onMapClick", beforeLocation + "");
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        map.clear();
+        if (courseEdit.getVisibility() == View.VISIBLE) {
+            beforeLocation = latLng;
+            Log.d("onMapClick", beforeLocation + "");
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            map.clear();
 
-        map.addMarker(markerOptions);
-
-        polylineOptions = new PolylineOptions();
-        polylineOptions.color(Color.RED);
-        polylineOptions.width(5);
-        polylineOptions.addAll(arrayPoints);
-        if (arrayPoints.size() > 0) {
-            polylineOptions.add(arrayPoints.get(0));
-        }
-        map.addPolyline(polylineOptions);
-
-        for (int i = 0; i < arrayPoints.size(); i++) {
-            markerOptions.position(arrayPoints.get(i));
-            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.round_location_on_black_24dp));
             map.addMarker(markerOptions);
+
+            polylineOptions = new PolylineOptions();
+            polylineOptions.color(Color.RED);
+            polylineOptions.width(5);
+            polylineOptions.addAll(arrayPoints);
+            if (arrayPoints.size() > 0) {
+                polylineOptions.add(arrayPoints.get(0));
+            }
+            map.addPolyline(polylineOptions);
+
+            for (int i = 0; i < arrayPoints.size(); i++) {
+                markerOptions.position(arrayPoints.get(i));
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.round_location_on_black_24dp));
+                map.addMarker(markerOptions);
+            }
         }
     }
 }
