71056e90e0202218fc2109fc7267b64cb2c3eb9e
이채린 <201821074@sangmyung.kr>
2020-06-22 23:41:45 +0900

    커뮤니티 기능 수정(미완성)

--- dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/community/PostAdapter.java
+++ dahada/app/src/main/java/com/d2w/dahada/data/activity_main/fragment_main/community/PostAdapter.java
@@ -3,12 +3,20 @@ package com.d2w.dahada.data.activity_main.fragment_main.community;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.d2w.dahada.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -28,10 +36,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
-        Post data = datas.get(position);
-        holder.documentid.setText(data.getDocumentId());
-        holder.title.setText(data.getTitle());
-        holder.contents.setText(data.getContents());
+        Post post = datas.get(position);
+        holder.documentid.setText(post.getDocumentId());
+        holder.title.setText(post.getTitle());
+        holder.contents.setText(post.getContents());
+
+        //Likes(holder.likes, post.getDocumentId());
     }
 
     @Override
@@ -44,6 +54,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         private TextView documentid;
         private TextView title;
         private TextView contents;
+        ImageView likes;
 
         public PostViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -51,6 +62,49 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
             documentid = itemView.findViewById(R.id.item_post_nickname);
             title = itemView.findViewById(R.id.item_post_title);
             contents = itemView.findViewById(R.id.item_post_contents);
+            likes = itemView.findViewById(R.id.likes);
         }
+    }
+
+    private void Likes(String documentid, final ImageView imageView){
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Likes")
+                .child(documentid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(firebaseUser.getUid()).exists()) {
+                    imageView.setImageResource(R.drawable.likes_fixed);
+                    imageView.setTag("liked");
+                } else {
+                    imageView.setImageResource(R.drawable.likes_unfixed);
+                    imageView.setTag("like");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void nLikes(final TextView likes, String documentid) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Likes")
+                .child(documentid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                likes.setText(dataSnapshot.getChildrenCount()+"likes");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
